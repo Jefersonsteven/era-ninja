193ec4090dfc15ea730c5256bed958e50006149a
gfrey <gfreycode@gmail.com>
2024-04-25 08:16:32 -0500

    first commit

---
+++ app/components/Helloworld.tsx
@@ -0,0 +1,5 @@
+export const Helloworld = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Helloworld } from "./components/Helloworld";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Helloworld />
+    </main>
+  );
+}
